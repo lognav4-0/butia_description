54456f2b96c5ec4e1507e225471f47bac3b4a8e0
jardeldyonisio <jardel.dyonisio@hotmail.com>
2024-12-06 09:39:05 -0300

    :sparkles: new organization for velodyne sensor

--- sensors_description/launch/velodyne.launch.py
+++ sensors_description/launch/velodyne.launch.py
@@ -1,18 +1,17 @@
 import os
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
-from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+import yaml
 
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+import ament_index_python.packages
+import launch
+import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+
 
 def generate_launch_description():
-    # Declare arguments
+    # Declare argument for RViz
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
@@ -25,62 +24,55 @@ def generate_launch_description():
     # Initialize Arguments
     use_rviz = LaunchConfiguration("use_rviz")
 
-    # Launch Velodyne Driver and PointCloud Transform
-    launch_velodyne = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('velodyne_driver'), 'launch', 'velodyne_driver_node-VLP16-launch.py')
-        )
+    config_dir = ament_index_python.packages.get_package_share_directory('sensors_description')
+
+    # Velodyne driver node
+    driver_params_file = os.path.join(config_dir, 'config', 'VLP16-velodyne_driver_node-params.yaml')
+    velodyne_driver_node = launch_ros.actions.Node(package='velodyne_driver',
+                                                   executable='velodyne_driver_node',
+                                                   output='both',
+                                                   parameters=[driver_params_file])
+
+    # Velodyne transform node
+    convert_params_file = os.path.join(config_dir, 'config', 'VLP16-velodyne_transform_node-params.yaml')
+    with open(convert_params_file, 'r') as f:
+        convert_params = yaml.safe_load(f)['velodyne_transform_node']['ros__parameters']
+    convert_params['calibration'] = os.path.join(config_dir, 'params', 'VLP16db.yaml')
+    velodyne_transform_node = launch_ros.actions.Node(package='velodyne_pointcloud',
+                                                      executable='velodyne_transform_node',
+                                                      output='both',
+                                                      parameters=[convert_params])
+
+    # Velodyne laserscan node
+    laserscan_params_file = os.path.join(config_dir, 'config', 'default-velodyne_laserscan_node-params.yaml')
+    velodyne_laserscan_node = launch_ros.actions.Node(package='velodyne_laserscan',
+                                                      executable='velodyne_laserscan_node',
+                                                      output='both',
+                                                      parameters=[laserscan_params_file])
+
+    # Initialize RViz node if use_rviz is true
+    rviz_config_file = os.path.join(config_dir, 'rviz', 'velodyne.rviz')  # Path to your RViz config file
+    rviz_node = launch_ros.actions.Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(use_rviz),
     )
 
-    launch_velodyne_driver = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('velodyne_pointcloud'), 'launch', 'velodyne_transform_node-VLP16-launch.py')
-        )
+    # Return LaunchDescription
+    return LaunchDescription(
+        declared_arguments + [
+            velodyne_driver_node,
+            velodyne_transform_node,
+            velodyne_laserscan_node,
+            rviz_node,  # Add RViz node conditionally
+            launch.actions.RegisterEventHandler(
+                event_handler=launch.event_handlers.OnProcessExit(
+                    target_action=velodyne_driver_node,
+                    on_exit=[launch.actions.EmitEvent(
+                        event=launch.events.Shutdown())],
+                )),
+        ]
     )
-
-
-    # Pointcloud to Laserscan Node
-    pointcloud_to_laser = Node(
-        package='pointcloud_to_laserscan',
-        executable='pointcloud_to_laserscan_node',
-        remappings=[('cloud_in', 'velodyne_points'), ('scan', 'scan')],
-        parameters=[{
-            'target_frame': 'velodyne',
-            'transform_tolerance': 0.05,
-            'min_height': 0.0,
-            'max_height': 1.0,
-            'angle_min': -3.1415,
-            'angle_max': 3.1415,
-            'angle_increment': 0.0174,
-            'scan_time': 0.1,
-            'range_min': 0.45,
-            'range_max': 70.0,
-            'use_inf': True,
-            'inf_epsilon': 1.0
-        }],
-        name='pointcloud_to_laserscan',
-    )
-    
-    # RViz Configuration
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("logistic_description"), "config", "velodyne.rviz"]
-    )
-
-    rviz_node = Node(
-       package="rviz2",
-       executable="rviz2",
-       name="rviz2",
-       output="log",
-       arguments=["-d", rviz_config_file],
-       condition=IfCondition(use_rviz),
-    )
-
-    # Define the order of launch actions
-    nodes = [
-        launch_velodyne,
-        launch_velodyne_driver,
-        pointcloud_to_laser,
-        rviz_node,
-    ]
-
-    return LaunchDescription(declared_arguments + nodes)
